3825093948ca8290fabee1cf9f50189536ead6ae
Bruno de Macêdo Ferreira <bruno.macedo@g.globo>
2024-03-19 13:10:18 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+
+  useEffect(() => {
+    getUsers()
+  })
+
+
+  async function getUsers() {
+    const response = await axios.get('https://brunomacedo1.github.io/db.json')
+
+    console.log(response)
+  }
 
   return (
     <>
